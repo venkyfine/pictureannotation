0c0d1e235195415f894215ef4e8ad7e25a370c69
jayesh kanzariya <jayeshkanzariya@elitemindz.co>
2022-08-04 11:35:51 +0530

    Initial Commit

---
+++ PictureAnnotationPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureAnnotationPOC
+//
+//  Created by jayesh kanzariya on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
